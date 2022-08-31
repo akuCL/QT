6ab51fe1f61b33d4730dcb18c2fe282ac430bfea
aku <297018083@qq.com>
2022-08-31 21:24:14 +0800

    QT自带日志：限制文件大小，格式化输出

--- test-logQT/main.cpp
+++ test-logQT/main.cpp
@@ -4,8 +4,7 @@
 #include <QMutex>
 #include <QTextStream>
 #include <QTime>
-//方式二
-//首先，包含头文件
+#include <QThread>
 #include <QDebug>
 
 void outputMessage(QtMsgType type, const QMessageLogContext &context, const QString &msg)
@@ -31,10 +30,9 @@ void outputMessage(QtMsgType type, const QMessageLogContext &context, const QStr
         text = QString("Fatal:");
     }
     QString context_info = QString("File:(%1) Line:(%2)").arg(QString(context.file)).arg(context.line);
-    QString current_date_time = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss");
+    QString current_date_time = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss:zzz");
     QString current_date = QString("%1").arg(current_date_time);
-    QString message = QString("%1 %2 %3 %4").arg(current_date).arg(text).arg(context_info).arg(msg);
-
+    QString message = QString("%1 %2 \t%3\t%4").arg(current_date).arg(context_info).arg(text).arg(msg);
 
     //判断文件夹是否存在，不存在新建
     QString aFile = QDir::currentPath() + "/LogFile/" + QDateTime::currentDateTime().toString("yyyyMMdd");
@@ -44,14 +42,21 @@ void outputMessage(QtMsgType type, const QMessageLogContext &context, const QStr
         dir.mkdir(aFile);//只创建一级子目录，即必须保证上级目录存在
     }
 
-    QString current_time = QDateTime::currentDateTime().toString("yyyyMMddhh");
+    QString current_time = QDateTime::currentDateTime().toString("yyyy-MM-dd-hh");
     QFile file(aFile+"/log"+current_time+".txt");
-    file.open(QIODevice::WriteOnly | QIODevice::Append);
-    QTextStream text_stream(&file);
-    text_stream << message << "\r\n \r\n";
-
-    file.flush();
-    file.close();
+    if(file.size() > 4 * 1024 * 1024 )
+    {
+        file.copy(aFile+"/log"+QDateTime::currentDateTime().toString("yyyy-MM-dd-hh-mm-ss.zzz") + ".txt");
+        file.setFileName(aFile+"/log"+current_time+".txt");
+        file.remove();
+    }
+    if (file.open(QIODevice::WriteOnly | QIODevice::Append))
+    {
+        QTextStream text_stream(&file);
+        text_stream << message << "\r\n";
+        file.flush();
+        file.close();
+    }
     mutex.unlock();
 }
 
@@ -61,15 +66,16 @@ int main(int argc, char *argv[])
 
     qInstallMessageHandler(outputMessage);
 
-    //方式一：
-    //打印日志到文件中
-    qDebug("This is a debug message");
-    qWarning("This is a warning message");
-    qCritical("This is a critical message");
-    QString LogInfo="这是一个测试消息！";
-    qDebug() <<LogInfo ;
-    //qFatal(LogInfo.toLatin1().data());
-
+    for(int i = 0; i < 100 ; ++i)
+    {
+        qDebug("This is a debug message");
+        qWarning("This is a warning message");
+        qCritical("This is a critical message");
+        QString LogInfo="这是一个测试消息！";
+        qDebug() <<LogInfo ;
+        //qFatal(LogInfo.toLatin1().data());
+        QThread::msleep(10);
+    }
 
     return a.exec();
 }
